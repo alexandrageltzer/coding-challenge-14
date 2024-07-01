2af091642b5fb84efa31c68a49ab1a714b8d0e20
Alexandra Geltzer <geltzer@usf.edu>
2024-07-01 15:03:29 -0400

    Update loanPayment.js
    create loan payment calculation module

--- modules/loanPayment.js
+++ modules/loanPayment.js
@@ -1,1 +1,5 @@
+export function calculateLoanPayment(principal, rate, n) {
+    const monthlyRate = rate / 12;
+    return (principal * monthlyRate * Math.pow(1 + monthlyRate, n)) / (Math.pow(1 + monthlyRate, n) - 1);
+}
 
